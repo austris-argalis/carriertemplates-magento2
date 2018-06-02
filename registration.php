e70d9360e16f12f5141eb2d4c8cd8ed9701d6452
austris argalis <austris.argalis@gmail.com>
2018-06-02 19:47:41 +0300

    Register module

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Davay\CarrierTemplates
+ * @author austris
+ * @copyright Copyright (c) 2018
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Davay_CarrierTemplates',
+    __DIR__
+);
