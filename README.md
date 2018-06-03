ca10eac57ffc1f9ba5cea929591982c09bff8de8
austris argalis <austris.argalis@gmail.com>
2018-06-03 05:08:43 +0300

    Add Travis

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Shipment Templates Magento2
 ======
 
 [![license](https://img.shields.io/github/license/mashape/apistatus.svg)](LICENSE.md)
+[![Build Status](https://travis-ci.org/austris-argalis/carriertemplates-magento2.svg?branch=master)](https://travis-ci.org/austris-argalis/carriertemplates-magento2)
 
 ## Description
 Magento 2 module that will let merchant configure a separate e-mail template for each of the active carriers.
---
+++ Test/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+BUILD_DIR="/tmp/magento2"
+MODULE_DIR="${BUILD_DIR}/app/code/Davay/CarrierTemplates"
+
+echo "Install Magento ${MAGENTO_VERSION}"
+composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition:$MAGENTO_VERSION $BUILD_DIR
+
+# move module into vendor folder of magento installation
+mkdir -p $MODULE_DIR
+mv ./* $MODULE_DIR
+
+# travis has empty password
+mv $BUILD_DIR/dev/tests/integration/etc/install-config-mysql.travis.php.dist \
+   $BUILD_DIR/dev/tests/integration/etc/install-config-mysql.php
+
+composer --working-dir=${BUILD_DIR} install --ignore-platform-reqs --prefer-dist
+mysql -uroot -e 'CREATE DATABASE magento_integration_tests;'
